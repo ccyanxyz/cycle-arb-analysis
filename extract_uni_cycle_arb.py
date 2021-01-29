5a825cebcd35e24f22f7eb46512a04f0b6918c9f
ccyanxyz <ccyanxyz@gmail.com>
2021-01-29 22:50:34 +0800

    updat

---
+++ extract_uni_cycle_arb.py
@@ -0,0 +1,29 @@
+import json
+
+def is_uni_cycle(info):
+    path = info['path']
+    if len(path) < 6 or len(path) % 2 != 0:
+        return None
+    if path[0] != path[-1]:
+        return None
+    p = path[1:-1]
+    for i in range(0, len(p), 2):
+        if p[i] != p[i+1]:
+            return None
+    return info
+
+with open('/data/new_uni_arb_info', 'r') as f:
+    for line in f:
+        info = json.loads(line)
+        ret = is_uni_cycle(info)
+        if not ret:
+            continue
+        if info['tx']['to'] == '0x11111254369792b2Ca5d084aB5eEA397cA8fa48B':
+            with open('data/1inch_cycle.json', 'a') as f1:
+                f1.write(json.dumps(info)+'\n')
+        elif info['tx']['to'] == '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D':
+            with open('data/router_cycle.json', 'a') as f2:
+                f2.write(json.dumps(info)+'\n')
+        else:
+            with open('data/cycle.json', 'a') as f3:
+                f3.write(json.dumps(info)+'\n')
