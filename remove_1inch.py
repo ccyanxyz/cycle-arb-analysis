fdbf98120c3c9299e4099c6ed02561d8aabc5735
ccyanxyz <ccyanxyz@gmail.com>
2021-01-30 21:31:23 +0800

    update

---
+++ remove_1inch.py
@@ -0,0 +1,11 @@
+import json
+from web3.auto import w3
+
+with open('/data/new_uni_arb_info') as f:
+    for line in f:
+        info = json.loads(line)
+        print(int(info['receipt']['blockNumber'], 16))
+        if w3.toChecksumAddress(info['tx']['to']) == '0x11111254369792b2Ca5d084aB5eEA397cA8fa48B':
+            continue
+        with open('files/cycle_include_router.json', 'a') as f2:
+            f2.write(json.dumps(info)+'\n')
