198d3a9a9e1a320d04c3c3740263c3f7c27c11a1
Yan Chen <yanchen57@ee-tik-ws020.ethz.ch>
2021-01-30 04:09:21 +0100

    update

--- extract_uni_cycle_arb.py
+++ extract_uni_cycle_arb.py
@@ -1,4 +1,5 @@
 import json
+from web3.auto import w3
 
 def is_uni_cycle(info):
     path = info['path']
@@ -15,13 +16,14 @@ def is_uni_cycle(info):
 with open('/data/new_uni_arb_info', 'r') as f:
     for line in f:
         info = json.loads(line)
+        print(int(info['receipt']['blockNumber'], 16))
         ret = is_uni_cycle(info)
         if not ret:
             continue
-        if info['tx']['to'] == '0x11111254369792b2Ca5d084aB5eEA397cA8fa48B':
+        if info['tx']['to'] and w3.toChecksumAddress(info['tx']['to']) == '0x11111254369792b2Ca5d084aB5eEA397cA8fa48B':
             with open('data/1inch_cycle.json', 'a') as f1:
                 f1.write(json.dumps(info)+'\n')
-        elif info['tx']['to'] == '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D':
+        elif info['tx']['to'] and w3.toChecksumAddress(info['tx']['to']) == '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D':
             with open('data/router_cycle.json', 'a') as f2:
                 f2.write(json.dumps(info)+'\n')
         else:
