bf2a51592e2e560bbe4b5a52c777a6b1b17c84b3
ccyanxyz <ccyanxyz@gmail.com>
2021-02-06 21:07:04 +0800

    update

---
+++ get_router_frontrun.py
@@ -0,0 +1,28 @@
+import json
+
+def search_frontrun(start, end, data):
+    frontrun_tx = None
+    with open('data/cycle_include_router_with_tx_from1.json') as f:
+        for line in f:
+            info = json.loads(line)
+            bn = int(info['receipt']['blockNumber'], 16)
+            if bn < start:
+                continue
+            if bn > end:
+                break
+            if info['input'] == data:
+                frontrun_tx = info
+    return frontrun_tx
+
+stats = {}
+with open('data/failed_router_arbs.json') as f:
+    for line in f:
+        info = json.loads(line)
+        bn = int(info['blockNumber'])
+        f_tx = search_frontrun(bn - 20, end, info['input'])
+        if not f_tx:
+            continue
+        with open('data/frontruned_arb_info.json', 'a') as f1:
+            i = {'frontrun': f_tx, 'victim': info}
+        stats['frontrun'] += 1
+print(stats)
