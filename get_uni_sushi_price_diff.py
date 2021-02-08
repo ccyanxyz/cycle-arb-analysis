edb46c22027ec329e07733b1f2b5911f88d75067
ccyanxyz <ccyanxyz@gmail.com>
2021-02-08 20:17:00 +0800

    update

--- get_uni_sushi_price_diff.py
+++ get_uni_sushi_price_diff.py
@@ -6,20 +6,21 @@ sushi_pairs = json.load(open('data/sushi_pairs.json'))
 
 def get_pair(token0, token1):
     for pair in pairs:
-        if token0 == pair['token0']['symbol'] and token1 == pair['token1']['symbol']:
+        if token0 == pair['token0']['id'].lower() and token1 == pair['token1']['id'].lower():
             return pair['id']
-        if token1 == pair['token0']['symbol'] and token0 == pair['token1']['symbol']:
+        if token1 == pair['token0']['id'].lower() and token0 == pair['token1']['id'].lower():
             return pair['id']
     return None
 
 def get_sushi_pair(token0, token1):
     for pair in sushi_pairs:
-        if token0 == pair['token0']['symbol'] and token1 == pair['token1']['symbol']:
+        if token0 == pair['token0']['id'] and token1 == pair['token1']['id']:
             return pair['id']
-        if token1 == pair['token0']['symbol'] and token0 == pair['token1']['symbol']:
+        if token1 == pair['token0']['id'] and token0 == pair['token1']['id']:
             return pair['id']
     return None
 
+token2addr = {'WBTC':'0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', 'USDC':'0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', 'DAI':'0x6b175474e89094c44da98b954eedeac495271d0f', 'USDT':'0xdac17f958d2ee523a2206206994597c13d831ec7', 'YFI':'0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', 'LINK':'0x514910771af9ca656af840dff83e8264ecf986ca', 'SNX':'0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', 'BADGER':'0x3472a5a71965499acd81997a54bba8d852c6e53d', 'LON':'0x0000000000095413afc295d19edeb1ad7b71c952', 'DIGG':'0x798d1be841a82a273720ce31c822c61a67a601c3', 'UNI':'0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', 'COMP':'0xc00e94cb662c3520282e6f5717214004a7f26888', 'sUSD':'0x57ab1ec28d129707052df4df418d58a2d46d5f51', 'MKR':'0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', 'KP3R':'0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44', 'WETH':'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'}
 # counts = json.load(open('data/path_counts.json'))
 # top10 = list(counts.keys())[:10]
 top10 = ['WBTC-ETH',
@@ -51,11 +52,13 @@ for path in top10:
     token1 = addrs[1]
     if token1 == 'ETH':
         token1 = 'WETH'
-    pair1 = get_pair(token0, token1)
+    t0 = token2addr[token0]
+    t1 = token2addr[token1]
+    pair1 = get_pair(t0, t1)
     top10_path_pair_addrs.append(pair1)
     addr2symbols[pair1] = path
 
-    pair2 = get_sushi_pair(token0, token1)
+    pair2 = get_sushi_pair(t0, t1)
     pair2 = w3.toChecksumAddress(pair2)
     sushi_top10_path_pair_addrs.append(pair2)
     addr2symbols[pair2] = path
@@ -66,7 +69,6 @@ print(addr2symbols)
 stats = {}
 for symbol in top10:
     stats[symbol] = {}
-'''
 with open('data/blockwise_reserves', 'r') as f:
     idx = 0
     for line in f:
@@ -80,18 +82,19 @@ with open('data/blockwise_reserves', 'r') as f:
                 stats[name][blockNumber] = {'r0': 0, 'r1': 0}
             else:
                 try:
-                    stats[name][i] = stats[addr][str(bn-1)]
+                    stats[name][i] = stats[name][str(bn-1)]
                 except:
                     pass
         for addr in info[blockNumber].keys():
+            ca = w3.toChecksumAddress(addr)
             r0 = info[blockNumber][addr]['reserve0']
             r1 = info[blockNumber][addr]['reserve1']
-            if addr in top10_path_pair_addrs:
+            if ca in top10_path_pair_addrs:
                 name_ = addr2symbols[addr]
                 stats[name_][bn] = {'r0': r0, 'r1': r1}
         idx += 1
 json.dump(stats, open('data/10pair_blockwise_reserves.json', 'w'))
-'''
+
 sushi_stats = {}
 for symbol in top10:
     sushi_stats[symbol] = {}
